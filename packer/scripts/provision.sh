f28a9fa2d956b4372742bb79beffb8d99235f58e
Dan Harris <danharris1606@gmail.com>
2021-05-12 19:47:04 +0100

    Create ansible user on VM template
    This is so new VMs can immediately be set up by ansible once they are
    created. Rather than opening root SSH a non-privileged user is used which can
    sudo up to root.

--- packer/scripts/provision.sh
+++ packer/scripts/provision.sh
@@ -8,4 +8,13 @@ sed -i 's/name: debian/name: dnhrrs/' /etc/cloud/cloud.cfg
 echo 'datasource_list: [ NoCloud, ConfigDrive ]' > /etc/cloud/cloud.cfg.d/99_pve.cfg
 systemctl enable cloud-init
 
+groupadd -g 5000 ansible
+useradd -r -m -d /var/lib/ansible/ -u 5000 -g ansible ansible
+mkdir /var/lib/ansible/.ssh/
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGRLUzMqpXJdtlTFiyosSOcjZxVqvd1g1E4fNEkjPTjH ansible' > /var/lib/ansible/.ssh/authorized_keys
+chmod 0700 /var/lib/ansible/.ssh/
+chmod 0600 /var/lib/ansible/.ssh/authorized_keys
+chown -R ansible:ansible /var/lib/ansible/.ssh/
+echo 'ansible ALL=(root) NOPASSWD: ALL' > /etc/sudoers.d/ansible
+
 passwd -l packer
