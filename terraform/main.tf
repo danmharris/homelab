d34a2345ebf4bab8ee20054d4f6c9469fc4f990a
Dan Harris <danharris1606@gmail.com>
2021-06-19 15:38:52 +0100

    Import existing DNS records for 2 VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,22 @@ resource "proxmox_vm_qemu" "westie" {
   }
 }
 
+resource "powerdns_record" "westie" {
+  zone    = "dnhrrs.xyz"
+  name    = "westie.dnhrrs.xyz."
+  type    = "A"
+  ttl     = 3600
+  records = ["10.23.20.100"]
+}
+
+resource "powerdns_record" "westie-ptr" {
+  zone    = "23.10.in-addr.arpa"
+  name    = "100.20.23.10.in-addr.arpa."
+  type    = "PTR"
+  ttl     = 3600
+  records = ["westie.dnhrrs.xyz."]
+}
+
 resource "proxmox_vm_qemu" "scotty" {
   name        = "scotty"
   desc        = "Recursive DNS server"
@@ -48,4 +64,20 @@ resource "proxmox_vm_qemu" "scotty" {
     bridge = "vmbr0"
     tag    = 20
   }
+}
+
+resource "powerdns_record" "scotty" {
+  zone    = "dnhrrs.xyz"
+  name    = "scotty.dnhrrs.xyz."
+  type    = "A"
+  ttl     = 3600
+  records = ["10.23.20.101"]
+}
+
+resource "powerdns_record" "scotty-ptr" {
+  zone    = "23.10.in-addr.arpa"
+  name    = "101.20.23.10.in-addr.arpa."
+  type    = "PTR"
+  ttl     = 3600
+  records = ["scotty.dnhrrs.xyz."]
 }
