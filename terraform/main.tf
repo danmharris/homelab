1c60ae221ce2586cdb57a25e7978bd760af3f1bb
Dan Harris <danharris1606@gmail.com>
2021-09-21 14:05:45 +0100

    Add DNS records for HomeAssistant
    As is a managed VM it has been deployed outside of terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,19 @@ module "db_1_vm" {
   ssh_public_key = var.ssh_public_key
   memory         = 1024
 }
+
+resource "powerdns_record" "dns-a" {
+  zone    = "dnhrrs.xyz"
+  name    = "ha.dnhrrs.xyz."
+  type    = "A"
+  ttl     = 3600
+  records = ["10.23.30.5"]
+}
+
+resource "powerdns_record" "dns-ptr" {
+  zone    = "23.10.in-addr.arpa"
+  name    = "5.30.23.10.in-addr.arpa."
+  type    = "PTR"
+  ttl     = 3600
+  records = ["ha.dnhrrs.xyz."]
+}
