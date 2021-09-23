daffd02ae8d0942dfe1b7b25af2601b09ce059eb
Dan Harris <danharris1606@gmail.com>
2021-09-23 14:08:48 +0100

    Move DNS servers into separate terraform plan
    Previously had a dependency on DNS that meant the plan wouldn't work
    from scratch. Also added some looping just in case more need to be
    added.
    
    Other stuff
    * Using provisioners. The docs advise against it but its the simpler
    solution than having images for each service (for now). Ansible host
    checking needs disabling for this (to stop the prompt)
    * Moved provider config out into env vars as that seems to be the
    defacto way and leaves variables open for resources only

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-module "westie_vm" {
-  source         = "./modules/vm"
-  name           = "westie"
-  desc           = "Authoritative DNS server"
-  node           = "tangor"
-  ip             = "10.23.20.100"
-  ssh_public_key = var.ssh_public_key
-}
-
-module "scotty_vm" {
-  source         = "./modules/vm"
-  name           = "scotty"
-  desc           = "Recursive DNS server"
-  node           = "tangor"
-  ip             = "10.23.20.101"
-  ssh_public_key = var.ssh_public_key
-}
-
 module "db_0_vm" {
   source         = "./modules/vm"
   desc           = "MySQL server primary"
---
+++ terraform/roots/dns/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "2.7.1"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://tangor.dnhrrs.xyz:8006/api2/json"
+  pm_tls_insecure = true
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+locals {
+  authdns_ips = toset(["10.23.20.104"])
+  recdns_ips  = toset(["10.23.20.101"])
+}
+
+resource "random_pet" "authdns_name" {
+  for_each = local.authdns_ips
+  length = 2
+}
+
+resource "proxmox_vm_qemu" "authdns" {
+  for_each    = local.authdns_ips
+  name        = random_pet.authdns_name[each.value].id
+  desc        = "Authoritative DNS server"
+  target_node = "tangor"
+  clone       = "debian11"
+
+  cores = 1
+  memory = 512
+
+  agent = 1
+  boot  = "order=scsi0;ide2;net0"
+
+  os_type   = "cloud-init"
+  ipconfig0 = "ip=${each.value}/24,gw=10.23.20.1"
+  sshkeys   = var.ssh_public_key
+
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "10G"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = 20
+  }
+
+  provisioner "local-exec" {
+    working_dir = "../../../ansible/"
+    command = "ansible-playbook -l ${each.value} site.yaml"
+  }
+}
+
+resource "random_pet" "recdns_name" {
+  for_each = local.recdns_ips
+  length = 2
+}
+
+resource "proxmox_vm_qemu" "recdns" {
+  for_each    = local.recdns_ips
+  name        = random_pet.recdns_name[each.value].id
+  desc        = "Authoritative DNS server"
+  target_node = "tangor"
+  clone       = "debian11"
+
+  cores = 1
+  memory = 512
+
+  agent = 1
+  boot  = "order=scsi0;ide2;net0"
+
+  os_type   = "cloud-init"
+  ipconfig0 = "ip=${each.value}/24,gw=10.23.20.1"
+  sshkeys   = var.ssh_public_key
+
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "10G"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = 20
+  }
+
+  provisioner "local-exec" {
+    working_dir = "../../../ansible/"
+    command = "ansible-playbook -l ${each.value} site.yaml"
+  }
+}
