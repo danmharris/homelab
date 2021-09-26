a9847fc11453c6f45274597cd0c01bc3d72463ca
Dan Harris <danharris1606@gmail.com>
2021-09-26 19:35:58 +0100

    Add 2 kubernetes nodes

--- terraform/roots/main/kubernetes.tf
+++ terraform/roots/main/kubernetes.tf
@@ -1,6 +1,6 @@
 module "k8s_cluster" {
   source         = "../../modules/kubernetes"
   controller_ips = ["10.23.20.100"]
-  node_ips       = []
+  node_ips       = ["10.23.20.102","10.23.20.103"]
   ssh_public_key = var.ssh_public_key
 }
