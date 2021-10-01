955f779a28f395cb7cd7ce9b80a52f7c705c6348
Dan Harris <danharris1606@gmail.com>
2021-10-01 19:47:44 +0100

    Rebuild k8s cluster with calico as cni
    Does some nicer routing using BGP (which my router supports) and no
    overlay networking, as well as policies. Easier to just rebuild the
    whole cluster so also replaced IP addresses.
    
    The BGP routing negates the need for a load balancer so remove metallb
    and get ingress to use IP which will be advertised.

--- terraform/roots/main/kubernetes.tf
+++ terraform/roots/main/kubernetes.tf
@@ -1,7 +1,7 @@
 module "k8s_cluster" {
   source         = "../../modules/kubernetes"
-  controller_ips = ["10.23.20.100"]
-  node_ips       = ["10.23.20.102","10.23.20.103"]
+  controller_ips = ["10.23.20.30"]
+  node_ips       = ["10.23.20.31","10.23.20.32"]
   ssh_public_key = var.ssh_public_key
   template       = "debian10"
 }
