c5bdaf50e7591a62cd7fbfbb854b2535058b68b1
Dan Harris <danharris1606@gmail.com>
2021-10-01 21:53:05 +0100

    Add NFS node
    Main export will be for k8s dynamic volume provisioning. Needed a bit of
    manual config with the second disk which is bind mounted into the
    export.

--- terraform/roots/main/kubernetes.tf
+++ terraform/roots/main/kubernetes.tf
@@ -5,3 +5,64 @@ module "k8s_cluster" {
   ssh_public_key = var.ssh_public_key
   template       = "debian10"
 }
+
+// NFS node
+resource "random_pet" "nfs_name" {
+  length   = 2
+}
+
+resource "proxmox_vm_qemu" "nfs" {
+  name        = random_pet.nfs_name.id
+  desc        = "NFS server"
+  target_node = "tangor"
+  clone       = "debian11"
+
+  cores  = 2
+  memory = 1024
+
+  agent = 1
+  boot  = "order=scsi0;ide2;net0"
+
+  os_type   = "cloud-init"
+  ipconfig0 = "ip=10.23.20.14/24,gw=10.23.20.1"
+  sshkeys   = var.ssh_public_key
+
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "10G"
+  }
+
+  disk {
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "100G"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+    tag    = 20
+  }
+
+  provisioner "local-exec" {
+    working_dir = "../../../ansible/"
+    command     = "ansible-playbook -l 10.23.20.14 site.yaml"
+  }
+}
+
+resource "powerdns_record" "nfs_a" {
+  zone     = "dnhrrs.xyz"
+  name     = "${proxmox_vm_qemu.nfs.name}.vm.dnhrrs.xyz."
+  type     = "A"
+  ttl      = 3600
+  records  = [proxmox_vm_qemu.nfs.default_ipv4_address]
+}
+
+resource "powerdns_record" "vm_ptr" {
+  zone     = "23.10.in-addr.arpa"
+  name     = "${join(".", reverse(split(".", proxmox_vm_qemu.nfs.default_ipv4_address)))}.in-addr.arpa."
+  type     = "PTR"
+  ttl      = 3600
+  records  = ["${proxmox_vm_qemu.nfs.name}.vm.dnhrrs.xyz."]
+}
