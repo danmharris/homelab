0a6abe2ebc9b09807d74c41c0aeb16b1769e1509
Dan Harris <danharris1606@gmail.com>
2021-10-01 21:00:47 +0100

    Add third machine to k8s cluster
    A physical host this time, will do more of the heavy lifting.

--- terraform/roots/main/dns.tf
+++ terraform/roots/main/dns.tf
@@ -13,3 +13,19 @@ resource "powerdns_record" "dns-ptr" {
   ttl     = 3600
   records = ["ha.dnhrrs.xyz."]
 }
+
+resource "powerdns_record" "kiwi-a" {
+  zone    = "dnhrrs.xyz"
+  name    = "kiwi.dnhrrs.xyz."
+  type    = "A"
+  ttl     = 3600
+  records = ["10.23.20.11"]
+}
+
+resource "powerdns_record" "kiwi-ptr" {
+  zone    = "23.10.in-addr.arpa"
+  name    = "11.20.23.10.in-addr.arpa."
+  type    = "PTR"
+  ttl     = 3600
+  records = ["kiwi.dnhrrs.xyz."]
+}
