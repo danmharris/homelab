c23602d90b6fe82051597cb516e3c1dc87b9228a
Dan Harris <danharris1606@gmail.com>
2023-08-06 12:44:39 +0100

    Fix redirect url

--- terraform/roots/authentik/application_nextcloud.tf
+++ terraform/roots/authentik/application_nextcloud.tf
@@ -3,7 +3,7 @@ resource "authentik_provider_oauth2" "nextcloud" {
   client_id          = "7c29295fd7e35c03daf8e9e6145e6a55"
   authorization_flow = data.authentik_flow.default-authorization-flow.id
   client_secret      = data.sops_file.secrets.data["nextcloud"]
-  redirect_uris      = ["https://nextcloud.dnhrrs.xyz/apps/sociallogin/oauth/authentik"]
+  redirect_uris      = ["https://nextcloud.dnhrrs.xyz/apps/sociallogin/custom_oidc/authentik"]
   property_mappings  = data.authentik_scope_mapping.default_scopes.ids
   signing_key        = data.authentik_certificate_key_pair.generated.id
 }
